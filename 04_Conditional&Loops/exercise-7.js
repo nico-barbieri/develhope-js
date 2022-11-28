5a47fc8e339da79090096620e52548f8a178c9ce
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-11-28 19:52:36 +0100

    done ex04

--- 04_Conditional&Loops/exercise-7.js
+++ 04_Conditional&Loops/exercise-7.js
@@ -2,9 +2,9 @@ function sumUntil(maxValue, minValue = 1) { //to sum number in a range if needed
   let sum = 0;
   for (let i = minValue; i <= maxValue; i++) {
     sum += i;
-  }
+  };
   return sum;
-}
+};
 
 console.log(sumUntil(5));
 
