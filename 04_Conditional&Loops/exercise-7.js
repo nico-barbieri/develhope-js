d36876b3186464744453ed121969e6f6136954ae
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-11-28 12:27:48 +0100

    made ex04

--- 04_Conditional&Loops/exercise-7.js
+++ 04_Conditional&Loops/exercise-7.js
@@ -1,5 +1,11 @@
-function sumUntil(maxValue) {
-  // ...
+function sumUntil(maxValue, minValue = 1) { //to sum number in a range if needed
+  let sum = 0;
+  for (let i = minValue; i <= maxValue; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 console.log(sumUntil(5));
+
+console.log('Sum every number from 12 to 25: ' + sumUntil(25, 12));
