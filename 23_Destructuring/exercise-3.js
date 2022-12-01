6db9752334042b6d8b029937339d69091227b0cf
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-02 00:16:24 +0100

    done ex23

--- 23_Destructuring/exercise-3.js
+++ 23_Destructuring/exercise-3.js
@@ -1,12 +1,12 @@
-function isAdult(person) {
-  return person.age >= 18;
+function isAdult({age}) {
+  return age >= 18;
 }
 
 const person = {
   id: 1,
   firstName: 'Mario',
   lastName: 'Rossi',
-  age: 25
+  age: 25,
 };
 
 console.log(isAdult(person));
