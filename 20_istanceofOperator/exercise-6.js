6e05692b0dfeca335b7c56481c2e05d35de2a3b8
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-01 23:56:49 +0100

    done ex20

--- 20_istanceofOperator/exercise-6.js
+++ 20_istanceofOperator/exercise-6.js
@@ -18,7 +18,17 @@ class Circle {
 }
 
 class AreaCalculator {
-  // ...
+  static calculate(figure) {  
+    if (figure instanceof Square) {
+      return Math.pow(figure.side, 2);
+    } else if(figure instanceof Rectangle) {
+      return figure.width * figure.height;
+    } else if(figure instanceof Circle) {
+      return Math.pow(figure.radius, 2) * Math.PI;
+    } else {
+      console.warn('AreaCalculator can calculate only areas of: Square, Rectangle, Circle');
+    }
+  }
 }
 
 const square = new Square(4);
