38d3e6cc3d28348ed1b642ea76bf2dd974069e91
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-04 16:20:46 +0100

    done ex34

--- 34_Callbacks/exercise.js
+++ 34_Callbacks/exercise.js
@@ -1,1 +1,9 @@
+function printAsyncName(sayHello, name) {
+    setTimeout(() => {
+        sayHello();
+        setTimeout(() => console.log(`${name}!`), 2000);
+    }, 1000);
+}
 
+const sayHello = () => console.log("Hello");
+printAsyncName(sayHello, "Nico");
