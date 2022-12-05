dec9ed4b3212a94ecd4b960f9765ad6825c6f121
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-05 17:08:45 +0100

    done ex36

--- 36_Callbacks/exercise.js
+++ 36_Callbacks/exercise.js
@@ -1,1 +1,8 @@
+function repeatHello(func) {
+    const repeatFunc = setInterval(func, 1000);
+    setTimeout(() => {clearInterval(repeatFunc)}, 5000);
+}
 
+const sayHello = () => console.log('Hello!');
+
+repeatHello(sayHello);
