7bdbac1c1270d50ba0c6b69d56ccf476d3a6e61a
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-05 17:50:54 +0100

    done ex37

--- 37_Promise/exercise.js
+++ 37_Promise/exercise.js
@@ -1,1 +1,13 @@
 const number = 15;
+
+const graterThanTen = new Promise((resolve, reject) => {
+    if (number > 10) {
+        resolve(number);
+    } else {
+        reject(number)
+    }
+})
+
+graterThanTen
+    .then((num) => console.log(num))
+    .catch((err) => console.error(err))
