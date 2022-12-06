0a5192cc28bb81cb25d18f681db3ba7c5a7c42c5
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-06 14:19:39 +0100

    done ex41 and fixed 40

--- 41_LocalStorage/exercise.js
+++ 41_LocalStorage/exercise.js
@@ -3,3 +3,15 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function storeLocal(obj, key = 'key') {
+  localStorage.setItem(key, JSON.stringify(obj));
+};
+
+function getLocal(key) {
+  console.log(JSON.parse(localStorage.getItem(key)));
+  return JSON.parse(localStorage.getItem(key));
+}
+
+storeLocal(user, 'user');
+getLocal('user');
